9a1eaabd0e219f370d3de4707a7881506abc22b3
RomaDel1fe <romadelife@gmail.com>
2023-03-26 22:30:21 +0300

    Homework for the eighth lesson is ready

--- js/main.js
+++ js/main.js
@@ -1,70 +1,47 @@
-// Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
-function removeElement(array, item) {
-  if (!Array.isArray(array)) {
-    console.error("The first argument must be an array.");
-    return;
-  }
-
-  const index = array.indexOf(item);
-
-  if (index !== -1) {
-    array.splice(index, 1);
+// Написать функцию которая будет принимать массив в качестве аргумента.
+// Массив типа
+// const arr = [ [ [ [8] ] ] ] - вложенность может быть любой, в конце всегда цифра
+// И возвращает ФАКТОРИАЛ этого числа.
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
   } else {
-    console.error(`The ${item} is not present in the array.`);
+    return n * factorial(n - 1);
   }
 }
 
-const array = [1, 2, 3, 4, 5, 6, 7];
-removeElement(array, 5);
-console.log(array);
-
-// Реалізуйте функцію generateKey(length, characters), яка повертає рядок випадкових символів із набору characters довжиною length. span>
-function generateKey(length, characters) {
-  let result = '';
-
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    result += characters[randomIndex];
+function arraysFactorial(array) {
+  while (Array.isArray(array)) {
+    array = array[0];
   }
-
-  return result;
+  return factorial(array);
 }
 
-const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const arr = [ [ [ [8] ] ] ];
+console.log(arraysFactorial(arr));
 
-const key = generateKey(16, characters);
-console.log(key);
+// Створіть об’єкт calculator з трьома методами:
+// read() запитує два значення та зберігає їх як властивості об’єкта з іменами a та b відповідно.
+// sum() повертає суму збережених значень.
+// mul() множить збережені значення і повертає результат.
+const calculator = {
+  x: null,
+  y: null,
 
-// Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 'func(" hello world", ['l', 'd'])' поверне нам "heo wor". 
-// Вихідний рядок та символи для видалення задає користувач.
-function removeChars(inputStr, charsToRemove) {
-  const inputArray = inputStr.split('');
-  const filteredArray = inputArray.filter(char => !charsToRemove.includes(char));
-  const result = filteredArray.join('');
-  return result;
-}
+  read(){
+    this.x = parseFloat(prompt("Введіть значення a:"));
+    this.y = parseFloat(prompt("Введіть значення b:"));
+  },
 
-const result = removeChars("hello world", ['l', 'd']);
-console.log(result); 
+  sum(){
+    return this.x + this.y;
+  },
 
-// Написати функцію, яка приймає 1 параметр. з тим, що передали перший раз і т. д. Все це із замиканнями, наприклад:
-function sum(initValue) {
-  let total = initValue;
+  mul(){
+    return this.x * this.y;
+  },
+};
 
-  function addToTotal(value) {
-    total += value;
-    console.log(total);
-    return addToTotal;
-  }
-
-  console.log(total);
-  return addToTotal;
-}
-
-const sumInstance = sum(3);
-
-sumInstance(5);
-sumInstance(5);
-sumInstance(5);
-
-
+calculator.read();
+console.log(`Сума: ${calculator.sum()}`);
+console.log(`Добуток: ${calculator.mul()}`);
