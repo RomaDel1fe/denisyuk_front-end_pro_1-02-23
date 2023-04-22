745007ca0e6ab2b8e6ca7d1ce26b73b882ca809b
RomaDel1fe <romadelife@gmail.com>
2023-04-22 20:33:05 +0300

    Homework for the thirteen lesson 1pt. is ready

--- js/main.js
+++ js/main.js
@@ -1,89 +1,85 @@
-// 1)Напишите функцию.На вход функции подаётся список строк и нужно найти общий для всех этих строк префикс максимальной длины. 
-// Например, для списка ["programming", "product", "procrastination"] — ответом будет "pro"
-function findPrefix(strings){
+// Вам потрібно зробити конструктор сутності "Студент".
+// Студент має ім'я, прізвище, рік народження — це властивості. 
+// Є масив з оцінками, це також властивість. 
+// І є можливість отримати вік студента та його середній бал – це методи.
+// Ще у всіх Студентів є по масиву однакової довжини, у ньому 25 елементів, спочатку він не заповнений, але на 25 елементів. 
+// Це масив, в якому відзначається відвідуваність, щоразу коли ми викликаємо метод .present() на чергове порожнє місце, 
+// в масив записується true, коли викликаємо .absent() - записується false. 
+// Передбачте будь-який захист від того, щоб у масиві відвідуваності не могло бути більше 25 записів. 
+// Масив – це властивість, present та absent – ​​методи.
+// Останній метод: .summary(), перевіряє середню оцінку і середнє відвідування(кількістьВідвідин/кількістьЗанять), 
+// і якщо середня оцінка більше 90, а середнє відвідування більше 0.9, то метод summary повертає рядок "Молодець!", 
+// якщо одне з цих значень менше , то - "Добре, але можна краще ", якщо обидва нижче - "Редиска!".
+// Не забудьте після того, як напишите цей конструктор, створити 2-3 екземпляри (конкретних студентів) 
+// і показати використання цих методів.
 
-  if (!strings.length) {
-    return "";
+class Student {
+  constructor(name, surname, birthYear, grades) {
+    this.name = name;
+    this.surname = surname;
+    this.birthYear = birthYear;
+    this.grades = grades;
+    this.attendance = new Array(25).fill(null);
+    this.attendanceIndex = 0;
   }
-  
-  if (strings.length === 1) {
-    return strings[0];
+
+  getAge() {
+    const currentYear = new Date().getFullYear();
+    return currentYear - this.birthYear;
   }
-  
-  const shortestString = strings.reduce((shortest, current) => {
-    return current.length < shortest.length ? current : shortest;
-  }, strings[0]);
-  
-  for (let i = 0; i < shortestString.length; i++) {
-    for (let j = 0; j < strings.length; j++) {
-      if (strings[j][i] !== shortestString[i]) {
-        return shortestString.slice(0, i);
-      }
+
+  getAverageGrade() {
+    const sum = this.grades.reduce((acc, grade) => acc + grade, 0);
+    return sum / this.grades.length;
+  }
+
+  present() {
+    if (this.attendanceIndex < 25) {
+      this.attendance[this.attendanceIndex] = true;
+      this.attendanceIndex++;
+    } else {
+      console.log("Відвідуваність не може перевищувати 25 записів");
     }
   }
-  
-  return shortestString;
+
+  absent() {
+    if (this.attendanceIndex < 25) {
+      this.attendance[this.attendanceIndex] = false;
+      this.attendanceIndex++;
+    } else {
+      console.log("Відвідуваність не може перевищувати 25 записів");
+    }
+  }
+
+  getAverageAttendance() {
+    const attendedClasses = this.attendance.filter((item) => item === true).length;
+    return attendedClasses / this.attendanceIndex;
+  }
+
+  summary() {
+    const averageGrade = this.getAverageGrade();
+    const averageAttendance = this.getAverageAttendance();
+
+    if (averageGrade > 90 && averageAttendance > 0.9) {
+      return "Молодець!";
+    } else if (averageGrade <= 90 || averageAttendance <= 0.9) {
+      return "Добре, але можна краще";
+    } else {
+      return "Редиска!";
+    }
+  }
 }
 
-const strings = ["programming", "product", "procrastination"];
-const commonPrefix = findPrefix(strings);
-console.log(commonPrefix); // "pro"
+const student1 = new Student("Олексій", "Мережко", 1998, [90, 85, 92, 88, 79]);
+const student2 = new Student("Роман", "Денисюк", 2001, [95, 92, 89, 96, 87]);
 
-// 2)Напишите функцию которая превращает массив двухмерных массивов в массив трехмерных и наоборот.
-// Остаток добавляется в новый массив
+student1.present();
+student1.present();
+student1.absent();
+console.log(student1.summary()); // "Добре, але можна краще"
 
-function convertArray(arr) {
-  let result = [];
-  let temp = [];
-  let isThreeDimensional = Array.isArray(arr[0][0]) ? false : true;
-  let targetLength = isThreeDimensional ? 3 : 2;
-
-  for (let subArr of arr) {
-      for (let elem of subArr) {
-          temp.push(elem);
-          if (temp.length === targetLength) {
-              result.push(temp);
-              temp = [];
-          }
-      }
-  }
-  if (temp.length > 0) {
-      result.push(temp);
-  }
-  return result;
-}
-
-let input1 = [[1, 2], [3, 4], [5, 6]];
-let output1 = convertArray(input1);
-console.log(output1);  // [[1, 2, 3], [4, 5, 6]]
-
-let input2 = [[1, 2], [3, 4], [5, 6], [7]];
-let output2 = convertArray(input2);
-console.log(output2);  // [[1, 2, 3], [4, 5, 6], [7]]
-
-let input3 = [[1, 2, 3], [4, 5, 6]];
-let output3 = convertArray(input3);
-console.log(output3);  // [[1, 2], [3, 4], [5, 6]]
-
-function complicatedConvertArray(arr, targetLength) {
-  let result = [];
-  let temp = [];
-
-  for (let subArr of arr) {
-      for (let elem of subArr) {
-          temp.push(elem);
-          if (temp.length === targetLength) {
-              result.push(temp);
-              temp = [];
-          }
-      }
-  }
-  if (temp.length > 0) {
-      result.push(temp);
-  }
-  return result;
-}
-
-const arr = [[1, 2, 3], [4, 5, 6]];
-const output = complicatedConvertArray(arr, 5);
-console.log(output);  // [[1, 2, 3, 4, 5], [6]]
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+console.log(student2.summary()); // "Молодець!"
