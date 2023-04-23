9e1bf670c5862ad0d97e62b5c5a99c1c13c175ac
RomaDel1fe <romadelife@gmail.com>
2023-04-23 13:43:32 +0300

    Homework for the thirteen lesson 2pt. is ready

--- js/main.js
+++ js/main.js
@@ -70,7 +70,7 @@ class Student {
   }
 }
 
-const student1 = new Student("Олексій", "Мережко", 1998, [90, 85, 92, 88, 79]);
+const student1 = new Student("Олексій", "Мережко", 1998, [90, 85, 92, null, 79]);
 const student2 = new Student("Роман", "Денисюк", 2001, [95, 92, 89, 96, 87]);
 
 student1.present();
@@ -83,3 +83,68 @@ student2.present();
 student2.present();
 student2.present();
 console.log(student2.summary()); // "Молодець!"
+
+
+
+// Мережа фастфудів пропонує кілька видів гамбургерів:
+// маленький (50 тугриків, 20 калорій);
+// великий (100 тугриків, 40 калорій).
+
+// Гамбургер може бути з одним із декількох видів начинок:
+// сиром (+ 10 тугриків, + 20 калорій);
+// салатом (+ 20 тугриків, + 5 калорій);
+// картоплею (+ 15 тугриків, + 10 калорій).
+
+// Можна додати добавки:
+// посипати приправою (+15 тугриків, 0 калорій) - полити майонезом (+ 20 тугриків, +5 калорій).
+
+// Напишіть програму, яка розраховує вартість та калорійність гамбургера. Використовуйте ООП підхід.
+// (підказка: потрібен клас Гамбургер, константи, методи для вибору опцій та розрахунку потрібних величин)
+
+class Hamburger{
+  static SIZE_SMALL = { price: 50, calories: 20 };
+  static SIZE_LARGE = { price: 100, calories: 40 };
+  static STUFFING_CHEESE = { price: 10, calories: 20 };
+  static STUFFING_SALAD = { price: 20, calories: 5 };
+  static STUFFING_POTATO = { price: 15, calories: 10 };
+  static TOPPING_SAUCE = { price: 15, calories: 0 };
+  static TOPPING_MAYO = { price: 20, calories: 5 };
+
+  constructor(size, stuffing){
+    this.size = size;
+    this.stuffing = stuffing;
+    this.toppings = [];
+  }
+
+  addTopping(topping){
+    this.toppings.push(topping);
+  }
+
+  calculate(){
+    const toppingsCalories = this.toppings.reduce((sum, topping) => sum + topping.calories, 0);
+    return this.size.calories + this.stuffing.calories + toppingsCalories;
+  }
+
+  calculatePrice() {
+    const toppingsPrice = this.toppings.reduce((sum, topping) => sum + topping.price, 0);
+    return this.size.price + this.stuffing.price + toppingsPrice;
+  }
+}
+
+// маленький гамбургер з начинкою з сиру
+var hamburger = new Hamburger(Hamburger .SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+
+// добавка з майонезу
+hamburger.addTopping(Hamburger.TOPPING_MAYO);
+
+// запитаємо скільки там калорій
+console.log("Calories: " + hamburger.calculate ());
+
+// скільки коштує
+console.log("Price: " + hamburger.calculatePrice());
+
+// я тут передумав і вирішив додати ще приправу
+hamburger.addTopping(Hamburger .TOPPING_SAUCE);
+
+// А скільки тепер коштує?
+console.log("Price with sauce: " + hamburger.calculatePrice());
