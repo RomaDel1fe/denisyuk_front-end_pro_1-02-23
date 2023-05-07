a9a85a5e22f66b892980fc69d25b9c38c074e281
RomaDel1fe <romadelife@gmail.com>
2023-05-07 18:10:11 +0300

    Homework for the 15 lesson is ready

--- js/main.js
+++ js/main.js
@@ -1,21 +1,47 @@
-async function fetchAllCharacters() {
-  let characters = [];
-  let nextUrl = "https://swapi.dev/api/people/";
+// Также используя https://swapi.dev/ API нужно сделать следующее.
+// Делаете запросы на все фильмы первой трилогии звездных войн, для этого у вас есть
+// "https://swapi.dev/api/films/1/
+// Далее при успешном результате всех запросов -
+// Вам нужно сделать запросы на все API планет которые появлялись в этих фильмах и вывести в консоль название этих планет
+// Если хоть один запрос отвалился - делаете запрос на это API
+// "https://swapi.dev/api/starships/9/"
+// И выводите в консоль - "Имя корабля" все уничтожил
 
-  while (nextUrl) {
-    const response = await fetch(nextUrl);
-    const data = await response.json();
-    characters = characters.concat(data.results);
-    nextUrl = data.next;
+async function fetchFilms() {
+  const filmUrls = [
+    "https://swapi.dev/api/films/1/",
+    "https://swapi.dev/api/films/2/",
+    "https://swapi.dev/api/films/3/"
+  ];
+
+  try {
+    const filmPromises = filmUrls.map(url => fetch(url).then(res => res.json()));
+    const films = await Promise.all(filmPromises);
+    const planetUrls = films.flatMap(film => film.planets);
+    const uniquePlanetUrls = [...new Set(planetUrls)];
+
+    const planetPromises = uniquePlanetUrls.map(url =>
+      fetch(url)
+        .then(res => {
+          if (!res.ok) {
+            throw new Error(`Помилка при отриманні даних про планету: ${url}`);
+          }
+          return res.json();
+        })
+    );
+
+    try {
+      const planets = await Promise.all(planetPromises);
+      planets.forEach(planet => console.log(planet.name));
+    } catch (error) {
+      console.error('Помилка при отриманні даних про планети:', error);
+      const starshipResponse = await fetch("https://swapi.dev/api/starships/9/");
+      const starship = await starshipResponse.json();
+      console.log(`${starship.name} все знищив`);
+    }
+  } catch (error) {
+    console.error('Помилка при отриманні даних про фільми:', error);
   }
-
-  return characters;
 }
 
-fetchAllCharacters()
-  .then(characters => {
-    console.log(characters);
-  })
-  .catch(error => {
-    console.error("Помилка:", error);
-  });
+fetchFilms();
