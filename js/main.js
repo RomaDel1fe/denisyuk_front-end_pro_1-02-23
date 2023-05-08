48d466f9b73e2f9e96d26ae0b4773b98ab6abf3b
RomaDel1fe <romadelife@gmail.com>
2023-05-08 15:07:24 +0300

    Homework for the 18 lesson 2pt. is ready

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // Emoji
   const emojiContainer = document.querySelector('.emoji-container');
   const emojis = ['😀', '😁', '😂', '🤣', '😃'];
+  // Shop
+  const categories = [
+    { id: 1, name: "Електроніка" },
+    { id: 2, name: "Одяг" },
+    { id: 3, name: "Харчові продукти" },
+  ];
 
+  const products = [
+    { id: 1, categoryId: 1, name: "Навушники", description: "Навушники з відмінним звуком" },
+    { id: 2, categoryId: 1, name: "Телефон", description: "Смартфон з великим екраном" },
+    { id: 3, categoryId: 2, name: "Светр", description: "Теплий зимовий светр" },
+    { id: 4, categoryId: 2, name: "Джинси", description: "Стійкі до зносу джинси" },
+    { id: 5, categoryId: 3, name: "Хліб", description: "Смачний свіжий хліб" },
+    { id: 6, categoryId: 3, name: "Молоко", description: "Органічне молоко" },
+  ];
+
+  const categoriesContainer = document.querySelector(".categories");
+  const productsContainer = document.querySelector(".products");
+  const productDetailsContainer = document.querySelector(".product-details");
+
+  // Emoji
   function createEmojiElement(emoji) {
     const emojiElement = document.createElement('span');
     emojiElement.classList.add('emoji', 'noselect');
@@ -35,4 +56,53 @@ document.addEventListener('DOMContentLoaded', () => {
 
     emojiContainer.appendChild(emojiWrapper);
   });
+
+  // Shop
+  function showCategories() {
+    categories.forEach((category) => {
+      const categoryElement = document.createElement("button");
+      categoryElement.textContent = category.name;
+      categoryElement.addEventListener("click", () => showProducts(category.id));
+      categoriesContainer.appendChild(categoryElement);
+    });
+  }
+
+  function showProducts(categoryId) {
+    productsContainer.innerHTML = "";
+    productDetailsContainer.innerHTML = "";
+
+    products
+      .filter((product) => product.categoryId === categoryId)
+      .forEach((product) => {
+        const productElement = document.createElement("button");
+        productElement.textContent = product.name;
+        productElement.addEventListener("click", () => showProductDetails(product.id));
+        productsContainer.appendChild(productElement);
+      });
+  }
+
+  function showProductDetails(productId) {
+    productDetailsContainer.innerHTML = "";
+
+    const product = products.find((p) => p.id === productId);
+
+    const productName = document.createElement("h2");
+    productName.textContent = product.name;
+    productDetailsContainer.appendChild(productName);
+
+    const productDescription = document.createElement("p");
+    productDescription.textContent = product.description;
+    productDetailsContainer.appendChild(productDescription);
+
+    const buyButton = document.createElement("button");
+    buyButton.textContent = "Купити";
+    buyButton.addEventListener("click", () => {
+      alert(`Товар "${product.name}" куплено!`);
+      productsContainer.innerHTML = "";
+      productDetailsContainer.innerHTML = "";
+    });
+    productDetailsContainer.appendChild(buyButton);
+  }
+
+  showCategories();
 });
