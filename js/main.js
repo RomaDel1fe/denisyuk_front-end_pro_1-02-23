3fc43bb2437f94863dee828b965cb2a362bf4a81
RomaDel1fe <romadelife@gmail.com>
2023-05-23 11:07:37 +0300

    the functionality of saving order information has been improved

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // Shop
   const categories = [
     { id: 1, name: "Електроніка" },
@@ -7,10 +7,30 @@ document.addEventListener('DOMContentLoaded', () => {
   ];
 
   const products = [
-    { id: 1, categoryId: 1, name: "Навушники", description: "Навушники з відмінним звуком" },
-    { id: 2, categoryId: 1, name: "Телефон", description: "Смартфон з великим екраном" },
-    { id: 3, categoryId: 2, name: "Светр", description: "Теплий зимовий светр" },
-    { id: 4, categoryId: 2, name: "Джинси", description: "Стійкі до зносу джинси" },
+    {
+      id: 1,
+      categoryId: 1,
+      name: "Навушники",
+      description: "Навушники з відмінним звуком",
+    },
+    {
+      id: 2,
+      categoryId: 1,
+      name: "Телефон",
+      description: "Смартфон з великим екраном",
+    },
+    {
+      id: 3,
+      categoryId: 2,
+      name: "Светр",
+      description: "Теплий зимовий светр",
+    },
+    {
+      id: 4,
+      categoryId: 2,
+      name: "Джинси",
+      description: "Стійкі до зносу джинси",
+    },
     { id: 5, categoryId: 3, name: "Хліб", description: "Смачний свіжий хліб" },
     { id: 6, categoryId: 3, name: "Молоко", description: "Органічне молоко" },
   ];
@@ -20,13 +40,42 @@ document.addEventListener('DOMContentLoaded', () => {
   const productDetailsContainer = document.querySelector(".product-details");
   const orderFormContainer = document.querySelector(".order-form");
 
+  function createButton(text, clickHandler) {
+    const buttonElement = document.createElement("button");
+    buttonElement.textContent = text;
+    buttonElement.addEventListener("click", clickHandler);
+    return buttonElement;
+  }
+  
+  function appendElement(parent, elementType, text, clickHandler) {
+    const element = document.createElement(elementType);
+    element.textContent = text;
+    if (clickHandler) {
+      element.addEventListener("click", clickHandler);
+    }
+    parent.appendChild(element);
+    return element;
+  }
+  
+  function clearElement(element) {
+    element.innerHTML = "";
+  }
+  
+
   function showCategories() {
+    clearElement(categoriesContainer);
+    clearElement(productsContainer);
+    productsContainer.classList.remove('hide');
+    clearElement(productDetailsContainer);
+    
     categories.forEach((category) => {
-      const categoryElement = document.createElement("button");
-      categoryElement.textContent = category.name;
-      categoryElement.addEventListener("click", () => showProducts(category.id));
-      categoriesContainer.appendChild(categoryElement);
+      categoriesContainer.appendChild(createButton(category.name, () => showProducts(category.id)));
     });
+  
+    const myOrdersButton = document.getElementById('myOrders');
+    myOrdersButton.textContent = 'Мої Замовлення';
+    myOrdersButton.removeEventListener('click', showCategories);
+    myOrdersButton.addEventListener('click', showOrders);
   }
 
   function showProducts(categoryId) {
@@ -38,7 +87,9 @@ document.addEventListener('DOMContentLoaded', () => {
       .forEach((product) => {
         const productElement = document.createElement("button");
         productElement.textContent = product.name;
-        productElement.addEventListener("click", () => showProductDetails(product.id));
+        productElement.addEventListener("click", () =>
+          showProductDetails(product.id)
+        );
         productsContainer.appendChild(productElement);
       });
   }
@@ -63,21 +114,28 @@ document.addEventListener('DOMContentLoaded', () => {
     productDetailsContainer.appendChild(buyButton);
   }
 
+  function saveOrderToLocalStorage(order) {
+    let orders = JSON.parse(localStorage.getItem("orders")) || [];
+    orders.push(order);
+    localStorage.setItem("orders", JSON.stringify(orders));
+  }
+
   function showOrderForm(product) {
-    fetch('/components/form.html')
-      .then(response => response.text())
-      .then(data => {
+    fetch("/components/form.html")
+      .then((response) => response.text())
+      .then((data) => {
         orderFormContainer.innerHTML = data;
-  
-        document.getElementById('orderForm').addEventListener('submit', (e) => {
+
+        document.getElementById("orderForm").addEventListener("submit", (e) => {
           e.preventDefault();
-  
-          const buyerName = document.getElementById('buyerName').value;
-          const city = document.getElementById('city').value;
-          const newPostOffice = document.getElementById('newPostOffice').value;
-          const productQuantity = document.getElementById('productQuantity').value;
-          const orderComments = document.getElementById('orderComments').value;
-  
+
+          const buyerName = document.getElementById("buyerName").value;
+          const city = document.getElementById("city").value;
+          const newPostOffice = document.getElementById("newPostOffice").value;
+          const productQuantity =
+            document.getElementById("productQuantity").value;
+          const orderComments = document.getElementById("orderComments").value;
+
           if (buyerName && city && newPostOffice && productQuantity) {
             const message = `<span>Замовлено ${productQuantity} одиниць товару"${product.name}".</span>
               <span>Доставка в місто ${city}, на склад №${newPostOffice}.</span>
@@ -85,11 +143,67 @@ document.addEventListener('DOMContentLoaded', () => {
             orderFormContainer.innerHTML = `<p class="message">${message}</p>`;
             productsContainer.innerHTML = "";
             productDetailsContainer.innerHTML = "";
+            saveOrderToLocalStorage({
+              name: buyerName,
+              city,
+              postOffice: newPostOffice,
+              quantity: productQuantity,
+              comments: orderComments,
+              product,
+              date: new Date(),
+              id: Date.now(),
+            });
           } else {
-            alert('Будь ласка, заповніть всі обов’язкові поля.');
+            alert("Будь ласка, заповніть всі обов’язкові поля.");
           }
         });
       });
-  }  
+  }
+
+  function showOrders() {
+    categoriesContainer.innerHTML = "";
+    productsContainer.innerHTML = "";
+    productsContainer.classList.add('hide');
+  
+    let orders = JSON.parse(localStorage.getItem('orders')) || [];
+    
+    orders.forEach((order) => {
+      const orderElement = document.createElement("button");
+      orderElement.textContent = `Замовлення від ${new Date(order.date).toLocaleDateString()}`;
+      orderElement.addEventListener("click", () => showOrderDetails(order));
+      categoriesContainer.appendChild(orderElement);
+    });
+  
+    // Зміна тексту кнопки
+    const myOrdersButton = document.getElementById('myOrders');
+    myOrdersButton.textContent = 'Магазин';
+    myOrdersButton.removeEventListener('click', showOrders);
+    myOrdersButton.addEventListener('click', showCategories);
+  }
+  
+  function showOrderDetails(order) {
+    clearElement(productDetailsContainer);
+    
+    appendElement(productDetailsContainer, "p",
+      `Ім'я покупця: ${order.name}\n` +
+      `Місто: ${order.city}\n` +
+      `Відділення Нової Пошти: ${order.postOffice}\n` +
+      `Кількість товару: ${order.quantity}\n` +
+      `Коментарі: ${order.comments}\n` +
+      `Товар: ${order.product.name}, ${order.product.description}`);
+  
+    productDetailsContainer.appendChild(createButton("Видалити замовлення", () => deleteOrder(order.id)));
+  }
+  
+  function deleteOrder(orderId) {
+    let orders = JSON.parse(localStorage.getItem('orders')) || [];
+    orders = orders.filter(order => order.id !== orderId);
+    localStorage.setItem('orders', JSON.stringify(orders));
+    productDetailsContainer.innerHTML = "";
+    showOrders();
+  }
+  
+  document.getElementById('myOrders').addEventListener('click', showOrders);
+  
   showCategories();
 });
